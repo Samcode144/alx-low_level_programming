c35292b943545d6aaa3e6cc6ac7145afe7c55792
Samcode144 <samuel443824@gmail.com>
2022-12-08 17:55:26 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry
- * Return: 1
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	printf(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

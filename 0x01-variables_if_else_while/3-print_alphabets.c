9224b65728cacc91c140153dd6434a1c4304bad3
Samcode144 <samuel443824@gmail.com>
2022-12-11 12:44:37 -0800

    3-print_alphabets.c task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * Description: print the alpabet in lowercase
+ * and the in uppercase, follow by a new line
+ * Return: Alway 0
+ */
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'A';
+	while (c <= 'Z')
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

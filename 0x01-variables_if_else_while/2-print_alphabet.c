e5057d86ed4c2c7b220a9809af776c319c5f37e6
Samcode144 <samuel443824@gmail.com>
2022-12-11 08:43:15 -0800

    2-print_alphabet.c task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description: Get a random it with 5
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+	
+      

34e48604fa5351a314ca9f25a80d252e6bdb8648
Samcode144 <samuel443824@gmail.com>
2022-12-12 12:49:51 -0800

    7-print_tebahpla.ctask

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -4,8 +4,8 @@
 /**
  * main - main block
  * Description: print all single digit number of base 10
- * starting from 0, followed by new line
- * Return 0
+ * starting from 0, followed by a new line
+ * Return: 0
  */
 int main(void)
 {

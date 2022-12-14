77b4eb955f5969e2f7186612245f09674b7ed6e1
Samcode144 <samuel443824@gmail.com>
2022-12-14 15:55:02 -0800

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 3-main.c 3-islower.c -o 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "main"
 
 /**
  * _islower - Checks for lowercase character
@@ -11,8 +11,8 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-	
+
 	return (1);
 	}
-        return (0);
-}	
+	return (0);
+}

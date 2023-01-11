9de1a199640489751e53791b70cb0f00c4697e18
Samcode144 <samuel443824@gmail.com>
2023-01-10 17:07:55 -0800

    task2

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,11 +11,19 @@ char *_strdup(char *str)
 	char *aaa;
 	int i, r = 0;
 
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	aaa = malloc(sizeof(char) * (i + 1));
+
+	if (aaa == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
+	for (r = 0; str[r]; r++)
+		aaa[r] = str[r];
+
+	return (aaa);
 }
